e929c1ed637c945ad7e6b364210e201f6a1b7ea6
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-04-03 10:41:26 +0000

    Tailwind configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2CA9BB',
+        secondary: '#003044',
+        success: '#00B87C',
+        danger: '#E74C3C',
+        light: '#2CA9BB30',
+        glass: '#FFFFFF20',
+      },
+      boxShadow: {
+        light: '0px 0px 0px 5px #2CA9BB33;',
+      },
+      fontFamily: {
+        primary: ["'Poppins'", 'sans-serif'],
+        title: ["'Quicksand'", 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
