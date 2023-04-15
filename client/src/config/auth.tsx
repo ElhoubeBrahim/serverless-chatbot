90e2de00e1abd97c346129840a3bec7f0b9e9467
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-04-15 02:17:52 +0000

    Setup api endpoints configuration

--- client/src/config/auth.tsx
+++ client/src/config/auth.tsx
@@ -1,4 +1,5 @@
-import { Amplify } from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
+import api from '../secure/api';
 import awsconfig from '../secure/aws';
 
 Amplify.configure({
@@ -6,5 +7,18 @@ Amplify.configure({
     region: awsconfig.REGION,
     userPoolId: awsconfig.USER_POOL_ID,
     userPoolWebClientId: awsconfig.USER_POOL_APP_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: api.name,
+        endpoint: api.serverUrl,
+        custom_header: async () => {
+          return {
+            Authorization: `${(await Auth.currentSession()).getIdToken().getJwtToken()}`,
+          };
+        },
+      },
+    ],
   },
 });
---
+++ client/src/secure/api.example.tsx
@@ -0,0 +1,8 @@
+// Rename this file to api.tsx and fill in the values below
+export default {
+  serverUrl: 'https://<your-api-gateway-id>.execute-api.us-east-1.amazonaws.com',
+  name: '<your-api-gateway-name>',
+  paths: {
+    chatrooms: '/rooms',
+  },
+};
