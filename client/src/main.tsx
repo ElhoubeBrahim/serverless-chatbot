9097ab20c9611fb9454d8319665cd32d737bfac8
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-04-15 02:46:00 +0000

    Setup react query

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,12 +8,16 @@ import './config/auth';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 import { RecoilRoot } from 'recoil';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './config/react-query';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RecoilRoot>
-      <RouterProvider router={router} />
-      <ToastContainer />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <RouterProvider router={router} />
+        <ToastContainer />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
